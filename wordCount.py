1f93ff42b389011325bded5a3fc950ad83a218e5
Aaron Mendez <aamendez3@miners.utep.edu>
2019-08-30 20:26:13 -0600

    The python file is now counting words but need to fix it because it need to ommit already given words

--- wordCount.py
+++ wordCount.py
@@ -1,20 +1,35 @@
 import re #Regular Expression Tools
 import sys #Use command line arguments
 
-inputFile = sys.argv[1]
+def check(word,file):
+    newFile = open(file).read()
+    if re.match(word,newFile):
+        return False
+    else:
+        return True
 
+inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
+count = 0
+
+file = open(inputFile).read()
 file2 = open(outputFile,"w")
 
-with open(inputFile, 'r') as f:
-    for line in f:
-        for word in line.split():
-            if True:
-                file2.write(word)
-                print(word)
+print(file)
+for line in file.split():
+    print(line)
+    for word in line.splitlines():    
+        #if check(word,file2):
+            print(line)
+            count = file.count(word)
+            print(count)
+            file2.write(line+" "+str(count))
 
-
+#with open('words.txt','r') as f:
+ #   for line in f:
+  #      for word in line.split():
+   #        print(word)      
 file2.close()
 
 
