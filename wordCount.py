583e1952e641590b74aec1484e3b13850a163e2c
Aaron Mendez <aamendez3@miners.utep.edu>
2019-09-01 17:24:17 -0600

    Modifing the readme to explain the way my code works, and also put comments in my .py code

--- wordCount.py
+++ wordCount.py
@@ -4,27 +4,28 @@ import sys #Use command line arguments
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
-count = 0
+file = open(inputFile).read().lower() #Open and read the file in lowercase
+file2 = open(outputFile,'w') #Open the file and be ready to write
+newString = re.sub('[^a-zA-Z0-9]',' ',file) #Ignore anything that is not a letter or number
+sortedNewString = newString.split() #Spliting the word into a list
+sortedNewString.sort() #Sorting the list in alphabetical order
 
-file = open(inputFile).read().lower()
-file2 = open(outputFile,'w')
-newString = re.sub('[^a-zA-Z0-9]',' ',file)
-sortedNewString = newString.split()
-sortedNewString.sort()
+oneDicto = {} #Creating a Dictionary
 
-oneDicto = {}
-#print(sortedNewString)
-
-for word in sortedNewString:
+"""
+In this for loop, it's just couting the instances of every word
+and add the a plus one. 
+"""
+for word in sortedNewString: 
         if word in oneDicto.keys():
                 oneDicto[word] = oneDicto[word] + 1
         else:
                 oneDicto[word] = 1
 
 for word in oneDicto.keys():
-        file2.write(word+" "+str(oneDicto[word])+"\n")
+        file2.write(word+" "+str(oneDicto[word])+"\n") #Writing into the new file
         
 
-file2.close()
+file2.close() #close the file
 
 
